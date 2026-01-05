e1301b846745719550ddd36cfda47f4ce5a32b6f
Yoav Natan <yoavnatan.yn@gmail.com>
2026-01-05 12:42:16 +0200

    Update css files to station

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -25,11 +25,11 @@ export function AppHeader() {
 					E2E Demo
 				</NavLink>
 				<NavLink to="about">About</NavLink>
-				<NavLink to="car">Cars</NavLink>
+				<NavLink to="station">Stations</NavLink>
 				<NavLink to="chat">Chat</NavLink>
 				<NavLink to="review">Review</NavLink>
 
-                {user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
+				{user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
 
 				{!user && <NavLink to="auth/login" className="login-link">Login</NavLink>}
 				{user && (
