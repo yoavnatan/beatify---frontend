57e7176b01d4049795a3d312abd7466d1ba5f703
Yoav Natan <yoavnatan.yn@gmail.com>
2026-01-05 11:43:53 +0200

    Update station index page

---
+++ src/pages/StationIndex.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
+import { loadStations, addStation, updateStation, removeStation, addStationMsg } from '../store/actions/station.actions.js'
+
+import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
+import { userService } from '../services/user.js'
+import { stationService } from '../services/car/station.service.js'
+
+import { StationList } from '../cmps/StationList.jsx'
+import { StationFilter } from '../cmps/StationFilter.jsx'
+
+export function StationIndex() {
+
+    const [filterBy, setFilterBy] = useState(stationService.getDefaultFilter())
+    const stations = useSelector(storeState => storeState.stationModule.stations)
+
+    useEffect(() => {
+        loadStations(filterBy)
+    }, [filterBy])
+
+    async function onRemoveStation(stationId) {
+        try {
+            await removeStation(stationId)
+            showSuccessMsg('Station removed')
+        } catch (err) {
+            showErrorMsg('Cannot remove station')
+        }
+    }
+
+    async function onAddStation() {
+        const station = stationService.getEmptyStation()
+        station.name = prompt('Name?', 'Some name')
+        try {
+            const savedStation = await addStation(station)
+            showSuccessMsg(`Station added (id: ${savedStation._id})`)
+        } catch (err) {
+            showErrorMsg('Cannot add station')
+        }
+    }
+
+    async function onUpdateStation(station) {
+        // const speed = +prompt('New speed?', station.speed) || 0
+        // if(speed === 0 || speed === station.speed) return
+
+        const stationToSave = { ...station, }
+        try {
+            const savedStation = await updateStation(stationToSave)
+            // showSuccessMsg(`Station updated, new speed: ${savedStation.speed}`)
+        } catch (err) {
+            showErrorMsg('Cannot update station')
+        }
+    }
+
+    return (
+        <section className="station-index">
+            <header>
+                <h2>Stations</h2>
+                {userService.getLoggedinUser() && <button onClick={onAddStation}>Add a Station</button>}
+            </header>
+            <StationFilter filterBy={filterBy} setFilterBy={setFilterBy} />
+            <StationList
+                stations={stations}
+                onRemoveStation={onRemoveStation}
+                onUpdateStation={onUpdateStation} />
+        </section>
+    )
+}
