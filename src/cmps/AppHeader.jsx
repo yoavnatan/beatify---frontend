bc13ab7a07e6a880bd1a45e48ce5048cb7dcf023
Yoav Natan <yoavnatan.yn@gmail.com>
2026-01-05 13:36:58 +0200

    Delete unnecassary files

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -27,7 +27,6 @@ export function AppHeader() {
 				<NavLink to="about">About</NavLink>
 				<NavLink to="station">Stations</NavLink>
 				<NavLink to="chat">Chat</NavLink>
-				<NavLink to="review">Review</NavLink>
 
 				{user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
 
