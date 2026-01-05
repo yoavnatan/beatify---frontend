8c496dc027133709e13093f754c741aca375b9cf
Yoav Natan <yoavnatan.yn@gmail.com>
2026-01-05 11:49:43 +0200

    Update station details page

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -8,7 +8,7 @@ import { ReviewIndex } from './pages/ReviewIndex.jsx'
 import { ChatApp } from './pages/Chat.jsx'
 import { AdminIndex } from './pages/AdminIndex.jsx'
 
-import { CarDetails } from './pages/CarDetails'
+import { CarDetails } from './pages/StationDetails.jsx'
 import { UserDetails } from './pages/UserDetails'
 
 import { AppHeader } from './cmps/AppHeader'
