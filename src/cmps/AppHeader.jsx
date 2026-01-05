d2854acf4778c843c85ab36919c9ba33c7605913
yahavmor <yahavmor77@gmail.com>
2026-01-05 20:37:19 +0200

    Styling app header and library,  importing fonts and unique colors

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -7,53 +7,74 @@ import { logout } from '../store/actions/user.actions'
 import logoImg from '../assets/img/logo.png'
 
 export function AppHeader() {
-	const user = useSelector(storeState => storeState.userModule.user)
-	const navigate = useNavigate()
+    const user = useSelector(storeState => storeState.userModule.user)
+    const navigate = useNavigate()
 
-	async function onLogout() {
-		try {
-			await logout()
-			navigate('/')
-			showSuccessMsg(`Bye now`)
-		} catch (err) {
-			showErrorMsg('Cannot logout')
-		}
-	}
+    async function onLogout() {
+        try {
+            await logout()
+            navigate('/')
+            showSuccessMsg(`Bye now`)
+        } catch (err) {
+            showErrorMsg('Cannot logout')
+        }
+    }
 
-	return (
-		<header className="app-header full">
-			<nav>
-				<NavLink to="/" className="logo">
-					<img src={logoImg} />
-				</NavLink>
-				<NavLink to="/"><i className="fa-solid fa-house"></i></NavLink>
-				<div className="search-wrapper">
-				<i className="fa-solid fa-magnifying-glass"></i>
-				<input
-					type="text"
-					className="search-input"
-					placeholder="What do you want to play?"
-				/>
-				<i className="fa-solid fa-basket-shopping"></i>
-				</div>
-				<NavLink to="about">About</NavLink>
-				{/* <NavLink to="station">Stations</NavLink> */}
-				<NavLink to="chat">Chat</NavLink>
+    return (
+        <header className="app-header full">
+            <nav className="header-nav">
 
-				{user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
+                <div className="nav-left">
+                    <NavLink to="/" className="logo">
+                        <img src={logoImg} alt="Logo" />
+                    </NavLink>
 
-				{!user && <NavLink to="auth/login" className="login-link">Login</NavLink>}
-				{user && (
-					<div className="user-info">
-						<Link to={`user/${user._id}`}>
-							{user.imgUrl && <img src={user.imgUrl} />}
-							{user.fullname}
-						</Link>
-						<span className="score">{user.score?.toLocaleString()}</span>
-						<button onClick={onLogout}>logout</button>
-					</div>
-				)}
-			</nav>
-		</header>
-	)
+                    {user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
+                </div>
+
+                <div className="nav-center">
+					<NavLink to="/" className="nav-icon">
+                        <i className="fa-solid fa-house"></i>
+                    </NavLink>
+                    <div className="search-wrapper">
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                        <input
+                            type="text"
+                            className="search-input"
+                            placeholder="What do you want to play?"
+                        />
+                        <i className="fa-solid fa-basket-shopping"></i>
+                    </div>
+                </div>
+
+                <div className="nav-right">
+					<NavLink to="/about">About</NavLink>
+                    <NavLink to="/chat">Chat</NavLink>
+                    {!user && (
+                        <NavLink to="auth/login" className="login-link">
+                            Login
+                        </NavLink>
+                    )}
+
+                    {user && (
+                        <div className="user-info">
+                            <Link to={`user/${user._id}`} className="user-link">
+                                {user.imgUrl && <img src={user.imgUrl} alt="User" />}
+                                <span>{user.fullname}</span>
+                            </Link>
+
+                            <span className="score">
+                                {user.score?.toLocaleString()}
+                            </span>
+
+                            <button onClick={onLogout} className="logout-btn">
+                                Logout
+                            </button>
+                        </div>
+                    )}
+                </div>
+
+            </nav>
+        </header>
+    )
 }
