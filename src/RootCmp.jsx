405afc8fca98fd42d03ebae3e9e4a581a827a472
Yoav Natan <yoavnatan.yn@gmail.com>
2026-01-06 17:06:17 +0200

    Separate Login page from main app

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router'
+import { Routes, Route, Outlet } from 'react-router'
 
 import { HomePage } from './pages/HomePage'
 import { StationIndex } from './pages/StationIndex.jsx'
@@ -15,30 +15,41 @@ import { UserMsg } from './cmps/UserMsg.jsx'
 import { LoginSignup, Login, Signup } from './pages/LoginSignup.jsx'
 import { Library } from './cmps/Library.jsx'
 
-
-export function RootCmp() {
+function MainLayout() {
     return (
         <div className="main-container main-layout">
             <AppHeader />
             <UserMsg />
             <main>
                 <Library />
-                <Routes>
-                    <Route path="" element={<HomePage />} />
-
-                    <Route path="station" element={<StationIndex />} />
-                    <Route path="station/:stationId" element={<StationDetails />} />
-                    <Route path="user/:id" element={<UserDetails />} />
-                    <Route path="chat" element={<ChatApp />} />
-                    <Route path="admin" element={<AdminIndex />} />
-                    <Route path="auth" element={<LoginSignup />}>
-                        <Route path="login" element={<Login />} />
-                        <Route path="signup" element={<Signup />} />
-                    </Route>
-                </Routes>
+                <Outlet />
             </main>
             <AppFooter />
         </div>
+
+    )
+}
+
+
+export function RootCmp() {
+    return (
+
+        <Routes>
+            <Route path="auth" element={<LoginSignup />}>
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Signup />} />
+            </Route>
+
+            <Route element={<MainLayout />}>
+                <Route path="" element={<HomePage />} />
+                <Route path="station" element={<StationIndex />} />
+                <Route path="station/:stationId" element={<StationDetails />} />
+                <Route path="user/:id" element={<UserDetails />} />
+                <Route path="chat" element={<ChatApp />} />
+                <Route path="admin" element={<AdminIndex />} />
+            </Route>
+
+        </Routes>
     )
 }
 
