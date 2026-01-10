d969e5753e301145d91119cef0f8110ddac818ad
Yoav Natan <yoavnatan.yn@gmail.com>
2026-01-10 20:51:55 +0200

    Fix previews header bug

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -4,7 +4,7 @@ export function StationPreview({ station }) {
         <article className="station-preview">
             {/* <Link to={`/station/${station._id}`}>{station.name}</Link> */}
             <img src={station.songs[0]?.imgUrl} alt={station.name} />
-            <h3 className="station-name">{station.name}</h3>
+            <h3 className="station-name-preview">{station.name}</h3>
             <div className="btn-play">
                 <Play className="icon small black" />
             </div>
