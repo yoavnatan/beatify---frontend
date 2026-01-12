ca4d649f9b90f65de142cd22f7557a01b9a0adc4
Yoav Natan <yoavnatan.yn@gmail.com>
2026-01-12 17:26:46 +0200

    Design basic player

--- src/cmps/Player.jsx
+++ src/cmps/Player.jsx
@@ -2,31 +2,77 @@ import React, { useEffect, useState } from 'react'
 import { useRef } from 'react';
 import ReactPlayer from 'react-player'
 import { useDispatch, useSelector } from 'react-redux';
-import { SET_IS_PLAYING, SET_SRC } from '../store/reducers/player.reducer';
+import { SET_IS_PLAYING, SET_IS_SEEKING, SET_PLAYED, SET_SRC } from '../store/reducers/player.reducer.js';
 import Play from "../assets/svg/play.svg?react"
 import Pause from "../assets/svg/pause.svg?react"
+import PlayNext from "../assets/svg/play-next.svg?react"
+import PlayPrev from "../assets/svg/play-prev.svg?react"
+import Shuffle from "../assets/svg/shuffle.svg?react"
+import Repeat from "../assets/svg/repeat.svg?react"
 
 export function Player() {
     const playerRef = useRef(null);
-    const playing = useSelector(storeState => storeState.playerModule.playing)
-    const src = useSelector(storeState => storeState.playerModule.src)
+    const { playing, src, seeking, played } = useSelector(storeState => storeState.playerModule)
     const dispatch = useDispatch()
 
+    function handleSeekMouseDown() {
+        dispatch({ type: SET_IS_SEEKING, seeking: true })
+    }
+
+    const handleSeekChange = (event) => {
+        const inputTarget = event.target
+        dispatch({ type: SET_PLAYED, played: Number.parseFloat(inputTarget.value) })
+    };
+
+    const handleSeekMouseUp = (event) => {
+        const inputTarget = event.target
+        dispatch({ type: SET_IS_SEEKING, seeking: false })
+        if (playerRef.current) {
+            playerRef.current.currentTime = Number.parseFloat(inputTarget.value) * playerRef.current.duration;
+        }
+    };
+
+
+
     return (
-        <div className="player container">
-            <div className="btn-play" onMouseDown={() => {
-                dispatch({ type: SET_SRC, src: "https://www.youtube.com/watch?v=85qgguw11P4&list=RD85qgguw11P4&start_radio=1" })
-            }}
-                onMouseUp={() => dispatch({ type: SET_IS_PLAYING })
-                }>{(playing)
-                    ?
-                    <Pause className="icon small black" />
-                    :
-                    <Play className="icon small black" />}</div>
+        <section className="player container">
+
+            <div className="controls flex justify-between">
+                <Shuffle className="icon small" />
+                <PlayPrev className="icon small" />
+                <div className="btn-play" onMouseDown={() => {
+                    dispatch({ type: SET_SRC, src: "https://www.youtube.com/watch?v=1e8mzCW0nlU&list=RD85qgguw11P4&index=2" })
+                }}
+                    onMouseUp={() => dispatch({ type: SET_IS_PLAYING })
+                    }>{(playing)
+                        ?
+                        <Pause className="icon small black" />
+                        :
+                        <Play className="icon small black" />}
+                </div>
+                <PlayNext className="icon small" />
+                <Repeat className="icon small" />
+            </div>
+            <div className="seek">
+                <input
+                    id="seek"
+                    type="range"
+                    min={0}
+                    max={0.999999}
+                    step="any"
+                    value={played}
+                    onMouseDown={handleSeekMouseDown}
+                    onChange={handleSeekChange}
+                    onMouseUp={handleSeekMouseUp}
+
+                />
+            </div>
+
+
             <div style={{
                 visibility: 'hidden',
                 position: 'absolute',
-                top: '-100%'
+                bottom: '100%'
             }}>
                 <ReactPlayer
                     ref={playerRef}
@@ -36,6 +82,7 @@ export function Player() {
                     height="100%"
                     playing={playing}
                     muted={false}
+                    // onProgress={handleProgress}
                     config={{
                         youtube: {
                             color: 'white'
@@ -43,7 +90,7 @@ export function Player() {
                     }}
                 />
             </div>
-        </div>
+        </section>
     )
 }
 
--- src/store/reducers/player.reducer.js
+++ src/store/reducers/player.reducer.js
@@ -1,11 +1,18 @@
 export const SET_IS_PLAYING = 'SET_IS_PLAYING'
 export const SET_SRC = 'SET_SRC'
+export const SET_IS_SEEKING = 'SET_IS_SEEKING'
+export const SET_PLAYED = 'SET_PLAYED'
 
 const initialState = {
     playing: false,
     src: null,
     nowPlaying: null,
     muted: false,
+    seeking: false,
+    played: 0,
+    duration: 0,
+    loop: false,
+    playedSeconds: 0,
     queue: [],
 }
 
@@ -17,6 +24,9 @@ export function playerReducer(state = initialState, action) {
             break
         case SET_SRC:
             newState = { ...state, src: action.src }
+            break
+        case SET_IS_SEEKING:
+            newState = { ...state, seeking: action.seeking }
         default:
     }
     return newState
