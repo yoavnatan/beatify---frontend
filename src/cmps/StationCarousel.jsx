3d068e035e64c9127d8b43d5d69da241c05b11b5
Yoav Natan <yoavnatan.yn@gmail.com>
2026-01-14 21:37:36 +0200

    Add fade effect to carusel

--- src/cmps/StationCarousel.jsx
+++ src/cmps/StationCarousel.jsx
@@ -7,6 +7,9 @@ export function StationCarousel({ stations }) {
 
     const [showLeftArrow, setShowLeftArrow] = useState(false)
     const [showRightArrow, setShowRightArrow] = useState(true)
+    const [fadeLeft, setFadeLeft] = useState(0)
+    const [fadeRight, setFadeRight] = useState(1)
+    // const [isScrolled, setIsScrolled] = useState(true)
 
     // useEffect(() => {
     //     listRef.current.addEventListener('scroll', onScrollEvent)
@@ -14,12 +17,21 @@ export function StationCarousel({ stations }) {
     //     return () => listRef.current.removeEventListener('scroll', onScrollEvent)
 
     // }, [])
-
+    console.log(fadeRight)
     function onScrollEvent() {
+        if ((listRef.current.scrollWidth - (listRef.current.scrollLeft + listRef.current.offsetWidth + 1)) / 100 <= 1) setFadeRight((listRef.current.scrollWidth - (listRef.current.scrollLeft + listRef.current.offsetWidth + 1)) / 100)
+        // if ((listRef.current.scrollWidth - (listRef.current.scrollLeft + listRef.current.offsetWidth + 1)) / 100 <= 1) {
+        //     setFadeRight(prevFade => (prevFade - ((listRef.current.scrollWidth - (listRef.current.scrollLeft + listRef.current.offsetWidth + 1)) / 100)))
+        // }
+        if (listRef.current.scrollLeft / 100 <= 1) setFadeLeft(listRef.current.scrollLeft / 100)
         setShowLeftArrow(true)
         setShowRightArrow(true)
-        if (listRef.current.scrollLeft + listRef.current.offsetWidth + 1 >= listRef.current.scrollWidth) setShowRightArrow(false)
-        if (listRef.current.scrollLeft === 0) setShowLeftArrow(false)
+        if (listRef.current.scrollLeft + listRef.current.offsetWidth + 1 >= listRef.current.scrollWidth) {
+            setShowRightArrow(false)
+        }
+        if (listRef.current.scrollLeft === 0) {
+            setShowLeftArrow(false)
+        }
     }
     function scrollCarousel(side) {
         if (side === 'right') {
@@ -37,10 +49,15 @@ export function StationCarousel({ stations }) {
 
 
     }
-
-
     return (
+
         <section className="station-carousel" >
+            <div className="fade-left"
+                style={{ opacity: `${fadeLeft}` }} >
+            </div>
+            <div className="fade-right"
+                style={{ opacity: `${fadeRight}` }} >
+            </div>
             <h2 className="carusel-title">More of your taste</h2>
             {showLeftArrow && <div className="arrow-wrapper left " onClick={() => scrollCarousel('left')}  >
                 <Arrow className="icon small arrow-left" />
@@ -59,12 +76,12 @@ export function StationCarousel({ stations }) {
                                         : station.songs[0]?.imgUrl
                                 }
                                 alt={station.name}
-                                />                            
-                                    <div className="btn-play">
-                                            <Play className="icon small-medium black" />
-                                    </div>
+                            />
+                            <div className="btn-play">
+                                <Play className="icon small-medium black" />
+                            </div>
                         </div>
-                        
+
                         <div className="description ">{station.name}</div>
 
                     </li>
