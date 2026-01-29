350da9b7dc7bb98d155529f470cedef490c01029
Yoav Natan <yoavnatan.yn@gmail.com>
2026-01-29 12:24:13 +0200

    Add search for playlists and press space for toggle play

--- src/store/reducers/search.reducer.js
+++ src/store/reducers/search.reducer.js
@@ -4,11 +4,13 @@ export const SET_RESULTS = 'SET_RESULTS'
 export const SET_ARTIST_RESULTS = 'SET_ARTIST_RESULTS'
 export const UPDATE_RECENT_SEARCH = 'UPDATE_RECENT_SEARCH'
 export const CLEAR_RECENT_SEARCH = 'CLEAR_RECENT_SEARCH'
+export const SET_SEARCH_INPUT = 'SET_SEARCH_INPUT'
 
 const initialState = {
     searchResults: [],
     artistResults: [],
     recentSearch: loadFromStorage('recent-search') || [],
+    searchInput: '',
 }
 
 export function searchReducer(state = initialState, action) {
@@ -25,6 +27,9 @@ export function searchReducer(state = initialState, action) {
             break
         case CLEAR_RECENT_SEARCH:
             newState = { ...state, recentSearch: [] }
+            break
+        case SET_SEARCH_INPUT:
+            newState = { ...state, searchInput: action.searchInput }
             break
         default:
     }
